612895c8dfb8aa2a0d43faa17ee7c1f26f80dc83
Jordan Wallace <jwall@localhost.localdomain>
2024-07-17 16:09:11 -0400

    stage: respond 200

--- app/server.c
+++ app/server.c
@@ -53,8 +53,11 @@ int main() {
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 
-	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	int client = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 	printf("Client connected\n");
+
+    char* response = "HTTP/1.1 200 OK\r\n\r\n";
+    send(client, response, strlen(response), 0);
 
 	close(server_fd);
 
